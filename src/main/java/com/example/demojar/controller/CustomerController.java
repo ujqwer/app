3aa5edc10a77d9a99e8344158ceedc49e930a9aa
ujjwal mishra <49057926+ujjman@users.noreply.github.com>
2023-10-20 15:13:23 +0530

    we

--- src/main/java/com/example/demojar/controller/CustomerController.java
+++ src/main/java/com/example/demojar/controller/CustomerController.java
@@ -13,12 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
-    private static final List<Customer> CUSTOMERS = List.of(
-            new Customer(1L, "john", "doe", "john@javawhizz.com"),
-            new Customer(2L, "mary", "public", "mary@javawhizz.com"),
-            new Customer(3L, "elon", "musk","elon@javawhizz.com"),
-            new Customer(4L, "dunny","duncan","dunny@javawhizz.com")
-    );
+   
 //    @GetMapping
 //    public List<Customer> findAllCustomers(){
 //        return CUSTOMERS;
