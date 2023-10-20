501830be5a5c437adba8ca7d799757e1add0b491
ujjwal mishra <ujjwalsini.um@gmail.com>
2023-10-20 11:08:35 +0530

    first commit

---
+++ src/test/java/com/example/demojar/DemoJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
