57b026c6861ad30a708b0d26917e146fcb7ecfc6
ujjwal mishra <49057926+ujjman@users.noreply.github.com>
2023-10-20 15:36:19 +0530

    hhg

--- src/main/java/com/example/demojar/controller/CustomerController.java
+++ src/main/java/com/example/demojar/controller/CustomerController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
-   
+
 //    @GetMapping
 //    public List<Customer> findAllCustomers(){
 //        return CUSTOMERS;
