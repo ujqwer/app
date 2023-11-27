a5576a1ff682890d6d39438cee7f422184a8f50a
ujjwal mishra <indianbhaium@gmail.com>
2023-11-27 14:54:22 +0530

    changed cors in wsconfig

--- src/main/java/com/example/demojar/config/WsConfig.java
+++ src/main/java/com/example/demojar/config/WsConfig.java
@@ -18,6 +18,6 @@ public class WsConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 }
